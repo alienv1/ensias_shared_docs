cb86b9583e69215a9030b842212234492514ae84
abderrazaq <la3ta3ta@gmail.com>
2019-05-20 10:31:05 +0100

    filiere + elements page

--- src/main/java/com/ensias_docs/demo/web/FrontController.java
+++ src/main/java/com/ensias_docs/demo/web/FrontController.java
@@ -2,8 +2,10 @@ package com.ensias_docs.demo.web;
 
 import com.ensias_docs.demo.dao.ElementModuleRepo;
 import com.ensias_docs.demo.dao.FiliereRepo;
+import com.ensias_docs.demo.dao.ModuleRepo;
 import com.ensias_docs.demo.entities.ElementModule;
 import com.ensias_docs.demo.entities.Filiere;
+import com.ensias_docs.demo.entities.Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,35 +19,57 @@ import java.util.Optional;
 public class FrontController {
     @Autowired
     private ElementModuleRepo elementModuleRepo;
+    @Autowired
+    private FiliereRepo filiereRepo;
+    @Autowired
+    private ModuleRepo moduleRepo;
+
 
     @RequestMapping(value = "/home")
-    public String element(Model model){
+    public String element(Model model) {
         List<ElementModule> elementModules = elementModuleRepo.findAll();
         model.addAttribute("elements", elementModules);
         return "element";
     }
-
-    @Autowired
-    private FiliereRepo filiereRepo;
     @RequestMapping(value = "/index.html")
-    public String index(Model model){
+    public String index(Model model) {
         List<Filiere> filieres = filiereRepo.findAll();
         model.addAttribute("filieres", filieres);
         return "index";
     }
 
     @RequestMapping(value = "/filieres.html")
-    public String filiere(Model model,@RequestParam(name = "id", defaultValue = "1") Long id){
+    public String filiere(Model model, @RequestParam(name = "filiere", defaultValue = "1") Long id
+            , @RequestParam(name = "ann", defaultValue = "1") int ann) {
         Optional<Filiere> filiere = filiereRepo.findById(id);
         model.addAttribute("filiere", filiere.get());
+        int[] sem = new int[2];
+        if (ann == 2) {
+            sem[0] = 3;
+            sem[1] = 4;
+        } else if (ann == 3) {
+            sem[0] = 5;
+            sem[1] = 6;
+        } else {
+            sem[0] = 1;
+            sem[1] = 2;
+        }
+        model.addAttribute("sem", sem);
         return "filieres";
     }
 
 
     @RequestMapping(value = "/elements.html")
-    public String element(Model model,@RequestParam(name = "id", defaultValue = "1") Long id){
-        Optional<ElementModule> elementModule = elementModuleRepo.findById(id);
-        model.addAttribute("filiere", elementModule.get());
+    public String element(Model model,
+                          @RequestParam(name = "module", defaultValue = "1") Long id_module,
+                          @RequestParam(name = "filiere", defaultValue = "1") Long id_filiere,
+                          @RequestParam(name = "element", defaultValue = "-1") Long id_elem
+                          ) {
+        Module module = moduleRepo.findById(id_module).get();
+        Filiere filiere = filiereRepo.findById(id_filiere).get();
+        model.addAttribute("module", module);
+        model.addAttribute("filiere", filiere);
+        model.addAttribute("elem", id_elem);
         return "elements";
     }
 }
